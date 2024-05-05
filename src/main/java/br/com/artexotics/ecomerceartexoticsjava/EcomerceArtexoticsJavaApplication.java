f99998eb1de773420f5be769a84808b85370c987
PauloMarquesG <paulomarquesg06@gmail.com>
2024-05-05 00:51:59 -0300

    init

---
+++ src/main/java/br/com/artexotics/ecomerceartexoticsjava/EcomerceArtexoticsJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.artexotics.ecomerceartexoticsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceArtexoticsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceArtexoticsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/artexotics/ecomerceartexoticsjava/EcomerceArtexoticsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.artexotics.ecomerceartexoticsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceArtexoticsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
